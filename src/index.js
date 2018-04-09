34e8ae083cf0acdc52fabf5620c09ff913c1d676
Tomasz Paterka <tomekpaterka@gmail.com>
2018-04-09 21:12:33 +0200

    Add Hot Module Replacment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 import App from './containers/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+if (module.hot) {
+	module.hot.accept('./containers/App', () => {
+		const NextApp = require('./containers/App').default;
+		ReactDOM.render(
+			<AppContainer>
+			<NextApp />
+			</AppContainer>,
+			document.getElementById('app')
+		);
+	});
+}
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,10 @@ var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 var OptimizeJsPlugin = require('optimize-js-plugin');
 
 module.exports = {
-	entry: './src/index.js',
+	entry: [
+		'react-hot-loader/patch',
+		'./src/index.js'
+	],
 	output: {
 		path: path.resolve(__dirname, 'build'),
 		filename: 'app.bundle.js'
